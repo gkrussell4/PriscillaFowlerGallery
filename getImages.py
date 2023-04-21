2159a516fa0be31d2db11e9f176054d2119239e7
Garrett Russell <gkrussell4@gmail.com>
2023-04-21 03:29:32 -0700

    Added error handling + summary file to find duplicates if present

--- getImages.py
+++ getImages.py
@@ -26,19 +26,55 @@ def download_image(url, save_path, artist_name, art_piece_name):
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
 
+def save_failed_url(url):
+    with open('failed_urls.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([url])
+
+def save_skipped_row(row):
+    with open('info.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
+
+def save_summary(row_num, row, status):
+    with open('summary.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([row_num, row, status])
+
 def main():
     csv_file = 'image_urls.csv'
-    download_folder = Path(os.path.expanduser('~/Downloads/ArtsyImages'))
+    download_folder = Path(os.path.expanduser('~/Downloads'))
+
+    downloaded_urls = set()
 
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
-        for row in reader:
+        for row_num, row in enumerate(reader, start=1):
+            print(f"Processing row {row_num}: {row}")
+
+            if len(row) != 3:
+                print(f"Skipping row {row_num} due to missing data")
+                save_skipped_row(row)
+                save_summary(row_num, row, 'skipped (missing data)')
+                continue
+
             url, artist_name, art_piece_name = row
+
+            if url in downloaded_urls:
+                print(f"Skipping row {row_num} due to duplicate URL: {url}")
+                save_skipped_row(row)
+                save_summary(row_num, row, 'skipped (duplicate URL)')
+                continue
+
             try:
                 download_image(url, download_folder, artist_name, art_piece_name)
+                downloaded_urls.add(url)
                 print(f"Downloaded image '{artist_name}_{art_piece_name}' from {url}")
+                save_summary(row_num, row, 'success')
             except requests.exceptions.RequestException as e:
                 print(f"Failed to download image '{artist_name}_{art_piece_name}' from {url}: {e}")
+                save_failed_url(url)
+                save_summary(row_num, row, 'failed')
 
 if __name__ == '__main__':
     main()
